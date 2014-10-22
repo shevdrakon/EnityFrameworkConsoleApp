e23dc1d188b156d79f1b4a1bc3a8036a76a39c12
VadimS <spieler3@yandex.ru>
2014-10-22 16:23:25 +0400

    init

---
+++ EnityFrameworkConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnityFrameworkConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new OnlineShopEntities();
+
+            var k = -1;
+
+            var gps = context.GoodsPositions;
+            var gp = gps.First();
+            var good = gp.Good;
+
+            k = 100;
+
+            var customer = gp.Order.GoodsPositions.Last().Good.Description;
+        }
+    }
+}
